a7ae5113056ef549ff39a4fdb6e28e0a0e881573
RPG-Alex <mralexblood@outlook.com>
2026-05-15 10:54:28 +0800

    Added convenienance method `periodic_table` for returning all `Element`s

---
+++ src/periodic_table.rs
@@ -0,0 +1,36 @@
+use strum::VariantArray;
+
+use crate::Element;
+
+impl Element {
+    /// Returns all elements in atomic number order as a static slice.
+    #[must_use]
+    pub const fn periodic_table() -> &'static [Self] {
+        Self::VARIANTS
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use strum::IntoEnumIterator;
+
+    use crate::Element;
+
+    #[test]
+    fn test_periodic_table() {
+        let periodic_table = Element::periodic_table();
+
+        for (i, element) in Element::iter().enumerate() {
+            assert_eq!(periodic_table[i], element);
+        }
+    }
+    #[test]
+    fn test_periodic_table_bounds() {
+        let periodic_table = Element::periodic_table();
+
+        assert_eq!(periodic_table.len(), 118);
+        assert_eq!(periodic_table[0], Element::H);
+        assert_eq!(periodic_table[1], Element::He);
+        assert_eq!(periodic_table[117], Element::Og);
+}
+}
